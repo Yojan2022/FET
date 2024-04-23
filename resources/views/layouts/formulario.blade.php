7fa51cc3bc18e9ab26545c61f1187b0801276d0a
Sabogal22 <nsabogalgaitan@gmail.com>
2024-04-22 22:16:23 -0500

    endentacion del formulario

--- resources/views/layouts/formulario.blade.php
+++ resources/views/layouts/formulario.blade.php
@@ -1,22 +1,21 @@
 <header>		
-		<form class="d-flex" role="search" action="../modelo/consulta.php" method="post" id="">
-			<div class="col-md-7 col-md-pull-2 animate-box" width="100%">
-				<div class="row">
-					<div class="col-md-12">
-						<span>Nombre</span>
-						<div class="form-group">
-							<input type="text" class="form-control" placeholder="Nombre" name="Nombre" id="Nombre"  required>
-							<br><br><span>Apellidos</span>
-							<input type="text" class="form-control" placeholder="Apellidos" name="consultarDocumento" id="ConsultarDocumento"  required>
-						</div>
+	<form class="d-flex" role="search" action="../modelo/consulta.php" method="post" id="">
+		<div class="col-md-7 col-md-pull-2 animate-box" width="100%">
+			<div class="row">
+				<div class="col-md-12">
+					<span>Nombre</span>
+					<div class="form-group">
+						<input type="text" class="form-control" name="Nombre" id="Nombre"  required>
+						<br><br><span>Apellidos</span>
+						<input type="text" class="form-control" name="consultarDocumento" id="ConsultarDocumento"  required>
 					</div>
-					<div class="col-md-12">
-						<div class="form-group">
-							<input type="submit" value="Buscar" class="btn btn-primary btn-modify" name="btn-consultar">
-						</div>
+				</div>
+				<div class="col-md-12">
+					<div class="form-group">
+						<input type="submit" value="Buscar" class="btn btn-primary btn-modify" name="btn-consultar">
 					</div>
 				</div>
 			</div>
 		</div>
-		</form>
+	</form>
 </header>
