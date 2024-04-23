a31e9d4421178edb430f3ba1e396c95bcdcad5e2
Sabogal22 <nsabogalgaitan@gmail.com>
2024-04-22 22:07:51 -0500

    rutas y endentacion para le login

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Controller;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+/* Rutas antiguas - Ancient routes */
 
 Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/home', [App\Http\Controllers\Controller::class, 'index'])->name('home');
-Route::get('/login', [App\Http\Controllers\Controller::class, 'index_login'])->name('login');
+Route::get('/home', [Controller::class, 'index'])->name('home');
+/* Route::get('/login', [App\Http\Controllers\Controller::class, 'index_login'])->name('login'); */ /* Antiguo login no funcional, solo vista - Old login not functional, view only */
+
+/* Rutas nuevas - New Ruts */
+/* Rutas para el login - Routes to log in */
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
