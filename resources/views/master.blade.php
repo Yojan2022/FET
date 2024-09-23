66d7a73d3cdf9c1226ca59ed15ed8191a338d02e
Sabogal22 <nsabogalgaitan@gmail.com>
2024-09-22 20:39:03 -0500

    endentacion del master

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-        @include('layouts.partials.head')
+      @include('layouts.partials.head')
     </head>
 	<body>
 		@include('layouts.partials.nav')
 		@include('layouts.partials.carruself')
-        @include('layouts.partials.info')
-        @section('content')
-        @show
+    @include('layouts.partials.info')
+      @section('content')
+      @show
 		@include('layouts.partials.foto')
 		@include('layouts.partials.footer')
 		@include('layouts.partials.script')
