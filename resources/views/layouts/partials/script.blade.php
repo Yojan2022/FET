af18950c012febf48f9ed60f26b0112e1079d33c
Sabogal22 <nsabogalgaitan@gmail.com>
2024-09-22 21:01:57 -0500

    implementacion del sweetalert2

--- resources/views/layouts/partials/script.blade.php
+++ resources/views/layouts/partials/script.blade.php
@@ -6,3 +6,5 @@
 <script src="{{ asset('js/jquery.flexslider-min.js') }}"></script>
 <!-- Main -->
 <script src="{{ asset('js/main.js') }}"></script>
+<!-- Sweetalert 2 -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
