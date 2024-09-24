b930a36f3dc284d818307fa710afb53b5375a755
Sabogal22 <nsabogalgaitan@gmail.com>
2024-09-24 15:01:03 -0500

    actualizacion con las migraciones

--- app/Models/book.php
+++ app/Models/book.php
@@ -4,9 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class book extends Model
+class Book extends Model
 {
-    protected $table = 'books';
-    
-    protected $fillable = ['name', 'last_name', 'folio', 'page', 'record'];
+    /**
+     * Tabla asociada al modelo.
+     *
+     * @var string
+     */
+    protected $table = 'books'; // Define el nombre de la tabla correspondiente en la base de datos
+
+    /**
+     * Atributos que son asignables en masa.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',        // Nombre de la persona
+        'last_name',   // Apellidos de la persona
+        'folio',       // Número de folio en el que está registrado el libro
+        'page',        // Número de la página donde se encuentra el libro
+        'record'       // Número de partida o registro del libro
+    ];
 }
