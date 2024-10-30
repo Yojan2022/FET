287658ff1763bdac00aaea50a33860080fbb8300
Sabogal22 <nsabogalgaitan@gmail.com>
2024-10-30 15:29:03 -0500

    mejorado

--- resources/views/layouts/admin/partials/footer.blade.php
+++ resources/views/layouts/admin/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
-  <strong>Copyright &copy; 2024-202... <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+  <strong>Copyright &copy; 2024-202... <a href="#">ADSI.soft</a>.</strong>
   All rights reserved.
   <div class="float-right d-none d-sm-inline-block">
-    <b>Version</b> 1.0.10
+    <b>Version</b> 1.0.20
   </div>
 </footer>
