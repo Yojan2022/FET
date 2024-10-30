abb4d2e644b5e41d9120d8f6f69b22bba5a420f3
Sabogal22 <nsabogalgaitan@gmail.com>
2024-10-30 10:30:02 -0500

    endentacion del scripts

---
+++ resources/views/layouts/admin/partials/scripts.blade.php
@@ -0,0 +1,11 @@
+<script src="{{ asset('AdminLTE/plugins/jquery/jquery.min.js') }}"></script>
+
+<script src="{{ asset('AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+<script src="{{ asset('AdminLTE/dist/js/adminlte.js?v=3.2.0') }}"></script>
+
+<script src="{{ asset('AdminLTE/plugins/chart.js/Chart.min.js') }}"></script>
+
+<script src="{{ asset('AdminLTE/dist/js/demo.js') }}"></script>
+
+<script src="{{ asset('AdminLTE/dist/js/pages/dashboard3.js') }}"></script>
