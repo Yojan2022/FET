27448eb8d412ccf6e232bba9a89acd586bdb2368
Sabogal22 <nsabogalgaitan@gmail.com>
2024-11-03 17:00:22 -0500

    adaptacion del DataTable

--- resources/views/layouts/admin/partials/scripts.blade.php
+++ resources/views/layouts/admin/partials/scripts.blade.php
@@ -11,3 +11,17 @@
 <script src="{{ asset('AdminLTE/dist/js/pages/dashboard3.js') }}"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<!-- DataTables  & Plugins -->
+<script src="{{ asset('AdminLTE/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/jszip/jszip.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/pdfmake/pdfmake.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/pdfmake/vfs_fonts.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
