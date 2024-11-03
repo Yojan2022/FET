b4d0f10ed58a5f4c53c49819b6abeb88219d8895
Sabogal22 <nsabogalgaitan@gmail.com>
2024-11-03 17:03:53 -0500

    implemetacion del ico

--- resources/views/layouts/admin/partials/head.blade.php
+++ resources/views/layouts/admin/partials/head.blade.php
@@ -2,6 +2,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>BAPTIZED | Dashboard 3</title>
 
+<link href="{{ asset('images/icono.ico') }}" rel="icon">
+
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
 <link rel="stylesheet" href="{{ asset('AdminLTE/plugins/fontawesome-free/css/all.min.css') }}">
